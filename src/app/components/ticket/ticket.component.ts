4b1ed52e74efe855bb4085b28bd4ffa4302a52e1
le2xx <le2x@rambler.ru>
2018-08-13 16:44:44 +0300

    Added methods dateTransform

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TicketComponent implements OnInit {
   @Input() ticket: object;
+  stops: any = ['ПЕРЕСАДОК', 'ПЕРЕСАДКА', 'ПЕРЕСАДКИ', 'ПЕРЕСАДКИ'];
+  months: any = ['', 'янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+  week: any = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  dateTransform(date: string) {
+    const day: number = Number(date.substr(0, 2));
+    const month: number = Number(date.substr(3, 2));
+    const year: number = Number(date.substr(6, 2));
+    const dayWeek: any = new Date(year + 2000, month - 1, day).getDay();
+    return `${day} ${this.months[month]} 20${year}, ${this.week[dayWeek]}`;
   }
 
 }
